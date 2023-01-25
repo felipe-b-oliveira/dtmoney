dc366eb05feb982e0ee97357996ca205e7c240cf
Felipe Oliveira <felipe.boliveira@outlook.com>
2023-01-24 23:17:46 -0300

    feat: Adds 'MirageJS' mocked server

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,45 +1,53 @@
-import { Container } from "./styles";
+import {useEffect} from "react";
+import {Container} from "./styles";
 
 export function TransactionsTable() {
-  return (
-    <Container>
-        <table>
-            <thead>
-                <tr>
-                    <th>Título</th>
-                    <th>Valor</th>
-                    <th>Categoria</th>
-                    <th>Data</th>
-                </tr>
-            </thead>
 
-            <tbody>
-                <tr>
-                    <td>Aluguel</td>
-                    <td className="withdraw">R$700</td>
-                    <td>Moradia</td>
-                    <td>20/02/2022</td>
-                </tr>
-                <tr>
-                    <td>Desenvolvimento de app</td>
-                    <td className="deposit">R$12.000</td>
-                    <td>Desenvolvimento</td>
-                    <td>20/07/2022</td>
-                </tr>
-                <tr>
-                    <td>Criação de artes para Redes Sociais</td>
-                    <td className="deposit">R$800</td>
-                    <td>Design</td>
-                    <td>20/02/2022</td>
-                </tr>
-                <tr>
-                    <td>Luz</td>
-                    <td className="withdraw">R$170</td>
-                    <td>Energia</td>
-                    <td>20/09/2022</td>
-                </tr>
-            </tbody>
-        </table>
-    </Container>
-  );
+    useEffect(() => {
+        fetch('http://localhost:3000/api/transactions')
+            .then(response => response.json())
+            .then(data => console.log(data))
+    }, []);
+
+    return (
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Título</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>Aluguel</td>
+                        <td className="withdraw">R$700</td>
+                        <td>Moradia</td>
+                        <td>20/02/2022</td>
+                    </tr>
+                    <tr>
+                        <td>Desenvolvimento de app</td>
+                        <td className="deposit">R$12.000</td>
+                        <td>Desenvolvimento</td>
+                        <td>20/07/2022</td>
+                    </tr>
+                    <tr>
+                        <td>Criação de artes para Redes Sociais</td>
+                        <td className="deposit">R$800</td>
+                        <td>Design</td>
+                        <td>20/02/2022</td>
+                    </tr>
+                    <tr>
+                        <td>Luz</td>
+                        <td className="withdraw">R$170</td>
+                        <td>Energia</td>
+                        <td>20/09/2022</td>
+                    </tr>
+                </tbody>
+            </table>
+        </Container>
+    );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {App} from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {createServer} from "miragejs";
+import {App} from "./App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+createServer({
+    routes() {
+        this.namespace = 'api';
+
+        this.get('/transactions', () => {
+            return [
+                {
+                    id: 1,
+                    title: 'Transaction 1',
+                    amount: 480,
+                    type: 'deposit',
+                    category: 'Food',
+                    createdAt: new Date()
+                }
+            ]
+        })
+    },
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
