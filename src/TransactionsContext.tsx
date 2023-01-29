4bf49914d0b36a26eeddacb70824e62028ef7382
Felipe Oliveira <felipe.boliveira@outlook.com>
2023-01-29 01:46:58 -0300

    feat: Increments TransactionsContext.tsx

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -10,12 +10,35 @@ interface Transaction {
     createdAt: string;
 }
 
+// Forms of declare TransactionInput
+
+// interface TransactionInput {
+//     title: string;
+//     type: string;
+//     amount: number;
+//     category: string;
+// }
+
+// type TransactionInput = Pick<Transaction, 'title' | 'amount' | 'type' | 'category'>;
+
+type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>;
+
 // Allows children components inside TransactionsProvider component. (see App.tsx)
 interface TransactionProviderProps {
     children: ReactNode;
 }
 
-export const TransactionsContext = createContext<Transaction[]>([]);
+// Allows to pass createTransation method on Context.Provider
+interface TransactionsContextData {
+    transactions: Transaction[];
+    createTransaction: (transaction: TransactionInput) => void;
+}
+
+// Hack to Typescript not to be throwing errors about default value.
+// TODO: See if the community already have a solution for this.
+export const TransactionsContext = createContext<TransactionsContextData>(
+    {} as TransactionsContextData
+);
 
 export function TransactionsProvider({ children }: TransactionProviderProps) {
     const [transactions, setTransactions] = useState<Transaction[]>([]);
@@ -25,8 +48,12 @@ export function TransactionsProvider({ children }: TransactionProviderProps) {
             .then(response => setTransactions(response.data.transactions));
     }, []);
 
+    function createTransaction(transaction: TransactionInput) {
+        api.post('/transactions', transaction);
+    }
+
     return (
-        <TransactionsContext.Provider value={transactions}>
+        <TransactionsContext.Provider value={{ transactions, createTransaction }}>
             {children}
         </TransactionsContext.Provider>
     );
